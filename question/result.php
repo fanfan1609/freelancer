aaa893497db48324219221477c820363c3a898c2
dat <vqdat169@gmail.com>
2015-03-27 00:43:32 +0700

    add colorbox

--- question/result.php
+++ question/result.php
@@ -4,6 +4,7 @@ $root = realpath($_SERVER["DOCUMENT_ROOT"]);
 include "header.php";
 ?>
 <link rel="stylesheet" href="css/font-awesome.min.css">
+<link rel="stylesheet" href="css/colorbox.css">
 <style>
 #testbox{
     background: -moz-linear-gradient(top, rgba(0, 0, 0, 0.7) 0%, rgba(0, 0, 0, 0.7) 100%), url(images/00.jpg) repeat 0 0, url(images/00.jpg) repeat 0 0;
@@ -29,6 +30,21 @@ include "header.php";
 
 #img-load { 
   position:absolute; 
+}
+
+#show_result {
+    position:absolute; bottom:10;right:270;
+    background-color: #7EBB14;
+    border-radius: 4px;
+    color: #FFF;
+    font-weight: bold;
+    text-decoration: none;
+    text-transform: uppercase;
+    text-align: center;
+    padding: 7px 17px;
+    font-size: 17px;
+    background: none repeat scroll 0% 0% #7EBB14;
+    transition: all 0.2s linear 0s;
 }
 </style>
 <div id="cover"><i id="img-load" class="fa fa-spinner fa-spin fa-5x"></i></div>
@@ -63,7 +79,8 @@ include "header.php";
         <input type="hidden" id='email_sent' name='email_sent' value='' ?>
         <?php endif;?>
         <input type='hidden' name='is_send_mail' value='1'>
-        <a href="show_result.php" id="show_result">No, thanks. Just show me my results</a> 
+        <!-- <a href="show_result.php" id="show_result">No, thanks. Just show me my results</a>  -->
+        <button id="show_result">No, thanks. Just show me my results</button>
     </form>
     <!-- <div id="cover"><img src="images/loading.gif" id="img-load"></div> -->
 </div>
@@ -73,49 +90,71 @@ include "header.php";
 // include "$root/survey/test/footer.php";
 include "footer.php";
 ?>
+<script type="text/javascript" src="js/jquery.colorbox-min.js"></script>
 <script type="text/javascript">
 $(function(){
-    $("#result").submit(function(){
+    // $("#result").submit(function(){
+    //     if( !$("input[name='email']").val() ){
+    //         alert("Enter your email please");
+    //         return false;
+    //     }
+    // });
+
+    $("#btnValidate").click(function(event){
+        event.preventDefault();
         if( !$("input[name='email']").val() ){
             alert("Enter your email please");
             return false;
         }
+        $("#btnValidate").colorbox({
+            iframe:true, 
+            width:"80%", 
+            height:"500px",
+            href : "http://www.sonician.info/otto/handlers/form_handler.php",
+            data : $("#result").serialize()
+        });
     });
 
-    $("#show_result").click(function(e){
-        e.preventDefault();
-        email   = $("input[name='email']").val();
-        url     = $(this).attr("href");
-        $("#cover").fadeIn();
-        if( email ){
-            $("#email_sent").val(email);
-            $.ajax({
-                url : "sendmail.php",
-                type: 'POST',
-                data: $("#form_show_result").serialize(),
-                dataType : 'json',
-                success: function(data){
-                    if(data.success === true ){
-                        $("#url_sent").val( data.url );
-                        console.log(data.url);
-                        $.ajax({
-                            url : $("#result").attr('action'),
-                            type: 'POST',
-                            data: $("#result").serialize,
-                            // crossDomain: true,
-                            // dataType: 'jsonp',
-                            success: function(data){
-                                loading(url);
-                            }
-                        });
-                        // loading(url);
-                    }
-                }
-            });
-        } else {
-            loading(url);
-        }
+    $("#show_result").colorbox({
+        iframe:true, 
+        width:"80%", 
+        height:"500px",
+        href : "show_result.php"
     });
+    // $("#show_result").click(function(e){
+    //     e.preventDefault();
+    //     email   = $("input[name='email']").val();
+    //     url     = $(this).attr("href");
+    //     $("#cover").fadeIn();
+    //     if( email ){
+    //         $("#email_sent").val(email);
+    //         $.ajax({
+    //             url : "sendmail.php",
+    //             type: 'POST',
+    //             data: $("#form_show_result").serialize(),
+    //             dataType : 'json',
+    //             success: function(data){
+    //                 if(data.success === true ){
+    //                     $("#url_sent").val( data.url );
+    //                     console.log(data.url);
+    //                     $.ajax({
+    //                         url : $("#result").attr('action'),
+    //                         type: 'POST',
+    //                         data: $("#result").serialize,
+    //                         // crossDomain: true,
+    //                         // dataType: 'jsonp',
+    //                         success: function(data){
+    //                             loading(url);
+    //                         }
+    //                     });
+    //                     // loading(url);
+    //                 }
+    //             }
+    //         });
+    //     } else {
+    //         loading(url);
+    //     }
+    // });
 });
 
 function loading(url){
